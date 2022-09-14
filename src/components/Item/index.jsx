26b0d079e2c6522ff5f81751b480529663dc3cab
romansalom <roman.salom12@gmail.com>
2022-09-14 12:07:35 -0300

    entrega trabajo fianl

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ItemCount from '../ItemCount'
-import '../Item/styles.css';
+import './styles.css';
 import {useNavigate} from 'react-router-dom';
 
 const Item = ({product}) => {
@@ -16,15 +16,15 @@ const Item = ({product}) => {
 
             <div className="container">
             <div className="row">
-                <div className="col-12 col-lg-3">
+                <div className="col">
                     <div className="card">
 
-                                        <img src={product.image} className="card-img-top" width={250} alt="foto" onClick={handleNavigate}></img>
+                                        <img src={product.image} className="card-img-top" width={250} alt="foto" ></img>
                                         <div class="card-body">
                                             <h5 className="card-title">{product.title}</h5>
                                             <p className="card-text">{product.description}</p>
-                                            <p >sotck : {product.stock}</p>
-                                            <p href="./">ver mas</p>
+                                            <p >precio : {product.price}</p>
+                                            <p className='vermas' onClick={handleNavigate}>ver mas</p>
                                                      <ItemCount></ItemCount>                             
                                         </div>
                                     </div>
